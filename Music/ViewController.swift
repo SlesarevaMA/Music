ac1cd10a5b63b0607fa1a63c49295118815ff09a
SlesarevaMA <90049402+SlesarevaMA@users.noreply.github.com>
2022-03-29 17:22:51 +0300

    Initial Commit

---
+++ Music/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Margarita Slesareva on 29.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
